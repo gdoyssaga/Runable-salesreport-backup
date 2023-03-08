c4704522c2077fb8bc79e37e6ea7358b08faec4f
Suebkwan <suebkwan@gmail.com>
2023-03-08 13:58:44 +0700

    first commit

---
+++ src/app/pages/sell-report/sell-report.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SellReportComponent } from './sell-report.component';
+
+describe('SellReportComponent', () => {
+  let component: SellReportComponent;
+  let fixture: ComponentFixture<SellReportComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SellReportComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SellReportComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
